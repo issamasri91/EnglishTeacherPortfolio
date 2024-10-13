1cc2383cda7750804ac76e11d1392ddeda14262c
IssamElasri <elasriisssam@gmail.com>
2024-10-13 19:38:49 +0100

    git rid of some files

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from pages import admin, views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Example path
+    path('', views.home, name='index'),  # Example path
     # Add other URL patterns here
 ]
